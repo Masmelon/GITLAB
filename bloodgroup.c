a94bcdaef4e9fdb4837da90f12a1dc2182ec07a7
sorakrichs <65063833+sorakrichs@users.noreply.github.com>
2020-10-21 15:54:04 +0700

    Blood Na Rak

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[40], G[5], answer[10];
+{ char Name[100], G[5], answer[10];
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("What is your name? ");
     scanf("%s", Name);
     do
     {
@@ -44,5 +44,5 @@ void main()
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
-  printf("Goodbye\n");
+  printf("GOODBYE HAVE A GOOD TIME.\n");
 }
